03f847b8681098bd05e94f598c28bda5f2447d41
SergIvchenko <43060611+SergIvchenko@users.noreply.github.com>
2018-09-10 00:52:59 +0300

    Add files via upload

---
+++ js/animations.js
@@ -0,0 +1,6 @@
+init = function(){
+      jarallax = new Jarallax();
+      jarallax.addAnimation('.bg2',[{progress: "0%", top:"110%"}, {progress: "33%", top: "0%"}]);
+      jarallax.addAnimation('.bg3',[{progress: "33%", top:"110%"}, {progress: "66%", top: "0%"}]);
+      jarallax.addAnimation('.bg4',[{progress: "66%", top:"110%"}, {progress: "100%", top: "0%"}]);
+    }
